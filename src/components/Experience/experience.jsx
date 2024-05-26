61356a3fd144524c3bb45964418e38380ea36607
oeggy03 <oeggy03>
2024-05-26 21:22:02 +0800

    Improve SEO with descriptions and keywords. Update list of experience.

--- src/components/Experience/experience.jsx
+++ src/components/Experience/experience.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./experience.css"
+import hdb from "../../assets/hdb.png"
+import cfw from "../../assets/cfw.png"
 import nus from "../../assets/NUS.png"
 import eastspring from "../../assets/eastspring.png"
 import astar from "../../assets/astar.png"
@@ -8,6 +10,9 @@ import js from "../../assets/js.png"
 import mss from "../../assets/mss.png"
 import oop from "../../assets/oop.png"
 import python from "../../assets/python.png"
+import react from "../../assets/react.png"
+import css from "../../assets/css.png"
+import html from "../../assets/html.png"
 
 const Experience = () => {
     return (
@@ -16,9 +21,44 @@ const Experience = () => {
         <div className="expCards">
             <div className="expCard">
                 <div className="cardContent">
+                    <a href='https://www.hdb.gov.sg/cs/infoweb/homepage' target="_blank" rel="noreferrer noopener"><img className="companyIcon" src={hdb} alt=""/></a>
+                    <div className="expDescSec">
+                        <div className="expTitle">Data Analysis / Machine Learning Intern</div>
+                        <div className="expCompany">Housing Development Board (HDB)</div>
+                        <div className="expDates">May 2024 - Present</div>
+                        <div className="expDescription">Utilized machine learning, data analysis and software engineering to drive business processes in HDB.</div>
+                        <div className="expSkills">
+                            <div className="skillsIntro">Skills:</div>
+                            <a href='https://en.wikipedia.org/wiki/Python_(programming_language)' target="_blank" rel="noreferrer noopener"><img className="techStackIconExp python" src={python} alt=""/></a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div className="expCard">
+                <div className="cardContent">
+                    <a href='https://www.care4we.com/' target="_blank" rel="noreferrer noopener"><img className="companyIcon" src={cfw} alt=""/></a>
+                    <div className="expDescSec">
+                        <div className="expTitle">Tech Lead</div>
+                        <div className="expCompany">CareForWe (Healthtech startup) </div>
+                        <div className="expDates">Dec 2023 - Present</div>
+                        <div className="expDescription">Headed development of a video hosting and front facing business website.</div>
+                        <div className="expSkills">
+                            <div className="skillsIntro">Skills:</div>
+                            <a href='https://en.wikipedia.org/wiki/JavaScript' target="_blank" rel="noreferrer noopener"><img className="techStackIconExp js" src={js} alt=""/></a>
+                            <a href='https://en.wikipedia.org/wiki/React_(software)' target="_blank" rel="noreferrer noopener"><img className="techStackIconExp react" src={react} alt=""/></a>
+                            <a href='https://en.wikipedia.org/wiki/HTML' target="_blank" rel="noreferrer noopener"><img className="techStackIconExp html" src={html} alt=""/></a>
+                            <a href='https://en.wikipedia.org/wiki/CSS' target="_blank" rel="noreferrer noopener"><img className="techStackIconExp css" src={css} alt=""/></a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div className="expCard">
+                <div className="cardContent">
                     <a href='https://nus.edu.sg/' target="_blank" rel="noreferrer noopener"><img className="companyIcon" src={nus} alt=""/></a>
                     <div className="expDescSec">
-                        <div className="expTitle">Teaching Assistant for Programming Methodology (CS1101S)</div>
+                        <div className="expTitle">Teaching Assistant</div>
                         <div className="expCompany">National University of Singapore</div>
                         <div className="expDates">Aug 2023 - Present</div>
                         <div className="expDescription">Graded programming assignments and taught programming methodology concepts to students.</div>
