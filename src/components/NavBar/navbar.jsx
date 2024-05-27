715d6691ecf31d1c18e2d3decdaa662ed0292231
oeggy03 <oeggy03>
2024-05-27 11:40:03 +0800

    Improve the burger menu by adding animations.

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,56 +1,76 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import logo_nav from "../../assets/logo_navbar.png"
 import linkedin from "../../assets/linkedin.png"
 import github from "../../assets/github.png"
 import burger from "../../assets/burgermenu.png"
-import {Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 import Resume from "../../assets/Resume_TranHaThu.pdf"
 
 const Navbar = () => {
-  const [showBurgerMenu, setShowBurgerMenu] = useState(false);
+  const [burger_class, setBurgerClass] = useState("burger-bar unclicked");
+  const [menu_class, setMenuClass] = useState("burgerNavMenu hidden");
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+  const updateBurgerMenu = () => {
+    if (!isMenuClicked) {
+      setBurgerClass("burger-bar clicked")
+      setMenuClass("burgerNavMenu visible")
+    } else {
+      setBurgerClass("burger-bar unclicked")
+      setMenuClass("burgerNavMenu hidden")
+    }
+
+    setIsMenuClicked(!isMenuClicked)
+  }
 
   return (
     <nav className='navbar'>
       <div className='internalLinks'>
-        <Link to='homepage' activeClass="activeHome"spy={true} smooth={true}><img className="logo_nav" src={logo_nav} alt="Ha Thu" /></Link>
+        <Link to='homepage' activeClass="activeHome" spy={true} smooth={true}><img className="logo_nav" src={logo_nav} alt="Ha Thu" /></Link>
         <div className="allNavbarItems">
           <Link activeClass="active" to='aboutSection' spy={true} smooth={true} className="navbarItem">About</Link>
           <Link activeClass="active" to='experiencesSec' spy={true} smooth={true} className="navbarItem">Experience</Link>
           <Link activeClass="active" to='projectsSec' spy={true} smooth={true} className="navbarItem">Projects</Link>
           <Link activeClass="active" to='contactSec' spy={true} smooth={true} className="navbarItem">Contact</Link>
-          <a href={Resume} target='_blank'className="burgerMenuItem">Resume</a>
-          
+          <a href={Resume} target='_blank' className="burgerMenuItem">Resume</a>
+
         </div>
       </div>
       <div className="externalLinks">
         <a href='https://www.linkedin.com/in/tran-ha-thu' target="_blank" rel="noreferrer noopener">
-          <img className="linkedIn" src={linkedin} alt=""/>
+          <img className="linkedIn" src={linkedin} alt="" />
         </a>
         <a href='https://github.com/oeggy03' target="_blank" rel="noreferrer noopener">
-          <img className="github" src={github} alt=""/>
+          <img className="github" src={github} alt="" />
         </a>
       </div>
-      <img className="burgerMenuImg" src={burger} alt="Menu" onClick={() => setShowBurgerMenu(!showBurgerMenu)}/>
-      <div className="burgerNavMenu" style={{display: showBurgerMenu? 'flex' : 'none'}}>
+
+      <div className="burger-menu" onClick={updateBurgerMenu}>
+        <div className={burger_class} ></div>
+        <div className={burger_class} ></div>
+        <div className={burger_class} ></div>
+      </div>
+
+      <div className={menu_class}>
+        <Link activeClass="active" to='aboutSection' spy={true} smooth={true} className="burgerMenuItem" onClick={updateBurgerMenu}>About</Link>
+        <Link activeClass="active" to='experiencesSec' spy={true} smooth={true} className="burgerMenuItem" onClick={updateBurgerMenu}>Experience</Link>
+        <Link activeClass="active" to='projectsSec' spy={true} smooth={true} className="burgerMenuItem" onClick={updateBurgerMenu}>Projects</Link>
+        <Link activeClass="active" to='contactSec' spy={true} smooth={true} className="burgerMenuItem" onClick={updateBurgerMenu}>Contact</Link>
+        <a href={Resume} target='_blank' className="burgerMenuItem">Resume</a>
+      </div>
+      {/* <img className="burgerMenuImg" src={burger} alt="Menu" onClick={() => setShowBurgerMenu(!showBurgerMenu)}/> */}
+      {/* <div className="burgerNavMenu" style={{ display: showBurgerMenu ? 'flex' : 'none' }}>
         <div className="burgerMenuInternalLinks">
-          <Link activeClass="active" to='aboutSection' spy={true} smooth={true} className="burgerMenuItem" onClick={() => setShowBurgerMenu(false)}>About</Link>
+          <Link activeClass="active" to='aboutSection' spy={true} smooth={true} className="burgerMenuItem" onClick={updateBurgerMenu}>About</Link>
           <Link activeClass="active" to='experiencesSec' spy={true} smooth={true} className="burgerMenuItem" onClick={() => setShowBurgerMenu(false)}>Experience</Link>
           <Link activeClass="active" to='projectsSec' spy={true} smooth={true} className="burgerMenuItem" onClick={() => setShowBurgerMenu(false)}>Projects</Link>
           <Link activeClass="active" to='contactSec' spy={true} smooth={true} className="burgerMenuItem" onClick={() => setShowBurgerMenu(false)}>Contact</Link>
-          <a href={Resume} target='_blank'className="burgerMenuItem">Resume</a>
-        
+          <a href={Resume} target='_blank' className="burgerMenuItem">Resume</a>
+
         </div>
-        <div className="externalLinks">
-          <a href='https://www.linkedin.com/in/tran-ha-thu' target="_blank" rel="noreferrer noopener">
-            <img className="linkedIn" src={linkedin} alt=""/>
-          </a>
-          <a href='https://github.com/oeggy03' target="_blank" rel="noreferrer noopener">
-            <img className="github" src={github} alt=""/>
-          </a>
-        </div>
-      </div>
-      
+      </div> */}
+
     </nav>
   )
 }
